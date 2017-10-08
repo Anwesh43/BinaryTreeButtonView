aa57bd5b62e5c43325b71d0e3b1aa6114025d25a
Anwesh43 <anweshthecool0@gmail.com>
2017-10-09 00:54:28 +0530

    adding Button BinaryTreeButtonView

--- binarytreebutton/src/main/java/com/anwesome/ui/binarytreebutton/BinaryTreeButtonView.kt
+++ binarytreebutton/src/main/java/com/anwesome/ui/binarytreebutton/BinaryTreeButtonView.kt
@@ -1,5 +1,22 @@
 package com.anwesome.ui.binarytreebutton
 
+import android.content.Context
+import android.view.*
+import android.graphics.*
 /**
  * Created by anweshmishra on 09/10/17.
  */
+class BinaryTreeButtonView(ctx:Context,var n:Int = 0):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
