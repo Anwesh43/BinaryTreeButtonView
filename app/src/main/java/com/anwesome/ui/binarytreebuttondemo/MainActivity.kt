05e5cb23570eb0f8c6a895d8253beb57995c8404
Anwesh43 <anweshthecool0@gmail.com>
2017-10-09 02:18:31 +0530

    using binarytree in app

--- app/src/main/java/com/anwesome/ui/binarytreebuttondemo/MainActivity.kt
+++ app/src/main/java/com/anwesome/ui/binarytreebuttondemo/MainActivity.kt
@@ -2,11 +2,21 @@ package com.anwesome.ui.binarytreebuttondemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.anwesome.ui.binarytreebutton.BinaryTreeButtonView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BinaryTreeButtonView.create(this)
+        addListeners()
+        BinaryTreeButtonView.show()
+
+    }
+    private fun addListeners() {
+        for(i in 1..15) {
+            BinaryTreeButtonView.addListener { Toast.makeText(this,"${i} clicked",Toast.LENGTH_SHORT).show()}
+        }
     }
 }
